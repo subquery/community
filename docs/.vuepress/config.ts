13a156fb52e0510e8dd55dff5054db05de16a98b
James Bayly <46693720+jamesbayly@users.noreply.github.com>
2021-12-09 15:41:04 +1300

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -151,6 +151,35 @@ export default defineUserConfig<DefaultThemeOptions>({
           },
         ],
       },
+      "/bg/": {
+        selectLanguageName: "Bulgarian",
+        sidebar: [
+          {
+            text: "Blogs",
+            link: "/bg/blogs/",
+            children: glob
+              .sync("docs/bg/blogs/*.md")
+              .map((f: string) => f.substr(f.indexOf("/")))
+              .sort((a, b) => (a > b ? -1 : 1)),
+          },
+          {
+            text: "Customer Announcements",
+            link: "/bg/customer_announcements/",
+            children: glob
+              .sync("docs/bg/customer_announcements/*.md")
+              .map((f: string) => f.substr(f.indexOf("/")))
+              .sort((a, b) => (a > b ? -1 : 1)),
+          },
+          {
+            text: "Recaps",
+            link: "bg/recaps/",
+            children: glob
+              .sync("docs/bg/recaps/*.md")
+              .map((f: string) => f.substr(f.indexOf("/")))
+              .sort((a, b) => (a > b ? -1 : 1)),
+          },
+        ],
+      },
       "/de/": {
         selectLanguageName: "German",
         sidebar: [
@@ -172,7 +201,7 @@ export default defineUserConfig<DefaultThemeOptions>({
           },
           {
             text: "Recaps",
-            link: "/recaps/",
+            link: "/de/recaps/",
             children: glob
               .sync("docs/de/recaps/*.md")
               .map((f: string) => f.substr(f.indexOf("/")))
@@ -201,9 +230,38 @@ export default defineUserConfig<DefaultThemeOptions>({
           },
           {
             text: "Recaps",
-            link: "/recaps/",
+            link: "/es/recaps/",
             children: glob
               .sync("docs/es/recaps/*.md")
+              .map((f: string) => f.substr(f.indexOf("/")))
+              .sort((a, b) => (a > b ? -1 : 1)),
+          },
+        ],
+      },
+      "/fr/": {
+        selectLanguageName: "Indonesian",
+        sidebar: [
+          {
+            text: "Blogs",
+            link: "/fr/blogs/",
+            children: glob
+              .sync("docs/fr/blogs/*.md")
+              .map((f: string) => f.substr(f.indexOf("/")))
+              .sort((a, b) => (a > b ? -1 : 1)),
+          },
+          {
+            text: "Customer Announcements",
+            link: "/fr/customer_announcements/",
+            children: glob
+              .sync("docs/fr/customer_announcements/*.md")
+              .map((f: string) => f.substr(f.indexOf("/")))
+              .sort((a, b) => (a > b ? -1 : 1)),
+          },
+          {
+            text: "Recaps",
+            link: "/fr/recaps/",
+            children: glob
+              .sync("docs/fr/recaps/*.md")
               .map((f: string) => f.substr(f.indexOf("/")))
               .sort((a, b) => (a > b ? -1 : 1)),
           },
@@ -230,7 +288,7 @@ export default defineUserConfig<DefaultThemeOptions>({
           },
           {
             text: "Recaps",
-            link: "/recaps/",
+            link: "/id/recaps/",
             children: glob
               .sync("docs/id/recaps/*.md")
               .map((f: string) => f.substr(f.indexOf("/")))
@@ -259,7 +317,7 @@ export default defineUserConfig<DefaultThemeOptions>({
           },
           {
             text: "Recaps",
-            link: "/recaps/",
+            link: "/it/recaps/",
             children: glob
               .sync("docs/it/recaps/*.md")
               .map((f: string) => f.substr(f.indexOf("/")))
@@ -288,7 +346,7 @@ export default defineUserConfig<DefaultThemeOptions>({
           },
           {
             text: "Recaps",
-            link: "/recaps/",
+            link: "/ja/recaps/",
             children: glob
               .sync("docs/ja/recaps/*.md")
               .map((f: string) => f.substr(f.indexOf("/")))
@@ -317,7 +375,7 @@ export default defineUserConfig<DefaultThemeOptions>({
           },
           {
             text: "Recaps",
-            link: "/recaps/",
+            link: "/ko/recaps/",
             children: glob
               .sync("docs/ko/recaps/*.md")
               .map((f: string) => f.substr(f.indexOf("/")))
@@ -346,7 +404,7 @@ export default defineUserConfig<DefaultThemeOptions>({
           },
           {
             text: "Recaps",
-            link: "/recaps/",
+            link: "/ru/recaps/",
             children: glob
               .sync("docs/ru/recaps/*.md")
               .map((f: string) => f.substr(f.indexOf("/")))
@@ -375,7 +433,7 @@ export default defineUserConfig<DefaultThemeOptions>({
           },
           {
             text: "Recaps",
-            link: "/recaps/",
+            link: "/th/recaps/",
             children: glob
               .sync("docs/th/recaps/*.md")
               .map((f: string) => f.substr(f.indexOf("/")))
@@ -404,7 +462,7 @@ export default defineUserConfig<DefaultThemeOptions>({
           },
           {
             text: "Recaps",
-            link: "/recaps/",
+            link: "/tr/recaps/",
             children: glob
               .sync("docs/tr/recaps/*.md")
               .map((f: string) => f.substr(f.indexOf("/")))
@@ -433,7 +491,7 @@ export default defineUserConfig<DefaultThemeOptions>({
           },
           {
             text: "Recaps",
-            link: "/recaps/",
+            link: "/uk/recaps/",
             children: glob
               .sync("docs/uk/recaps/*.md")
               .map((f: string) => f.substr(f.indexOf("/")))
@@ -462,7 +520,7 @@ export default defineUserConfig<DefaultThemeOptions>({
           },
           {
             text: "Recaps",
-            link: "/recaps/",
+            link: "/vi/recaps/",
             children: glob
               .sync("docs/vi/recaps/*.md")
               .map((f: string) => f.substr(f.indexOf("/")))
@@ -491,7 +549,7 @@ export default defineUserConfig<DefaultThemeOptions>({
           },
           {
             text: "Recaps",
-            link: "/recaps/",
+            link: "/zh/recaps/",
             children: glob
               .sync("docs/zh/recaps/*.md")
               .map((f: string) => f.substr(f.indexOf("/")))
